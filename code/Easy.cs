9e319f6616fe519018903439fef8a516280ba231
CriggerMarg <anton.shakalo@live.com>
2020-06-17 18:05:39 +0300

    Two Sum

--- code/Easy.cs
+++ code/Easy.cs
@@ -171,5 +171,23 @@ namespace code
 
             return 0;
         }
+
+        /// <summary>
+        /// https://leetcode.com/problems/two-sum/
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static int[] TwoSum(int[] nums, int target)
+        {
+
+            for (int a = 0; a < nums.Length - 1; a++)
+            for (int b = a + 1; b < nums.Length; b++)
+            {
+                if (nums[a] + nums[b] == target)
+                    return new int[2] { a, b };
+            }
+            return new int[2] { 0, 1 };
+        }
     }
 }
--- tests/Easy.cs
+++ tests/Easy.cs
@@ -67,5 +67,13 @@ namespace tests
         {
             Assert.AreEqual(expected, LeetEasy.Reverse(input));
         }
+
+        [TestCase(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+        [TestCase(new[] { 3,2,4 }, 6, new[] { 1,2 })]
+
+        public void TwoSum(int[] input, int target, int[] expected)
+        {
+            Assert.AreEqual(expected, LeetEasy.TwoSum(input, target));
+        }
     }
 }
