3948c7404797ee98bc5473c9ab643163ca63ffd1
CriggerMarg <anton.shakalo@live.com>
2020-06-17 17:10:52 +0300

    Container With Most Water

--- tests/Medium.cs
+++ tests/Medium.cs
@@ -11,5 +11,13 @@ namespace tests
         public void Setup()
         {
         }
+
+        [TestCase(new[] { 1, 1 }, 1)]
+        [TestCase(new[] { 2, 3, 4, 5, 18, 17, 6 }, 17)]
+        [TestCase(new[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49)]
+        public void MaxArea(int[] height, int expected)
+        {
+            Assert.AreEqual(expected, LeetMed.MaxArea(height));
+        }
     }
 }
