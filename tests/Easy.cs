b31c66211e9d7764c435fa1a9bb76743797d71a3
CriggerMarg <anton.shakalo@live.com>
2020-06-17 17:59:39 +0300

    Roman to Integer

--- tests/Easy.cs
+++ tests/Easy.cs
@@ -30,11 +30,24 @@ namespace tests
         }
 
 
-        [TestCase(new[] {"flower", "flow", "flight"}, "fl")]
+        [TestCase(new[] { "flower", "flow", "flight" }, "fl")]
         [TestCase(new[] { "dog", "racecar", "car" }, "")]
         public void LongestCommonPrefix(string[] input, string expected)
         {
-            Assert.AreEqual(expected,LeetEasy.LongestCommonPrefix(input));
+            Assert.AreEqual(expected, LeetEasy.LongestCommonPrefix(input));
+        }
+
+
+        [TestCase("I", 1)]
+        [TestCase("II", 2)]
+        [TestCase("III", 3)]
+        [TestCase("IV", 4)]
+        [TestCase("IX", 9)]
+        [TestCase("LVIII", 58)]
+        [TestCase("MCMXCIV", 1994)]
+        public void RomanToInt(string input, int expected)
+        {
+            Assert.AreEqual(expected, LeetEasy.RomanToInt(input));
         }
     }
 }
