bf261bc6e03800806a4866b5c5d615ae14cee366
CriggerMarg <anton.shakalo@live.com>
2020-06-17 17:31:54 +0300

    Median of Two Sorted Arrays

--- code/Hard.cs
+++ code/Hard.cs
@@ -1,10 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace code
 {
     public static class Hard
     {
+        private static double ArrayMediana(int[] arr)
+        {
+
+            if (arr.Length % 2 == 1)
+                return arr[arr.Length / 2];
+            var left = arr[arr.Length / 2 - 1];
+            var right = arr[arr.Length / 2];
+            return 0.5 * (left + right);
+        }
+
+        public static double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var mrg = nums1.Concat(nums2).ToArray();
+            Array.Sort(mrg);
+            return ArrayMediana(mrg);
+        }
     }
 }
--- tests/Hard.cs
+++ tests/Hard.cs
@@ -10,6 +10,13 @@ namespace tests
         public void Setup()
         {
         }
-
+        [TestCase(new int[0] , new[] { 1 }, 1)]
+        [TestCase(new[] { 3 }, new[] { -2, -1 }, -1)]
+        [TestCase(new[] { 1, 3, }, new[] { 2 }, 2.0)]
+        [TestCase(new[] { 1, 2, }, new[] { 3, 4 }, 2.5)]
+        public void FindMedianSortedArrays(int[] left, int[] right, double expected)
+        {
+            Assert.AreEqual(expected, LeetHard.FindMedianSortedArrays(left, right));
+        }
     }
 }
